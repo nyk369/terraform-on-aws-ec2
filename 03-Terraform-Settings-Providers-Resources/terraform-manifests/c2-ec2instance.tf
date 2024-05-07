3d9a306920a4a006ce5bf4474dc732aa86185d38
it.soldier999 <it.soldier999@gmail.com>
2024-05-07 22:59:07 +0530

    updated

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -2,4 +2,8 @@
 resource "aws_instance" "test_ec2VM" {
   ami = "ami-013e83f579886baeb"
   instance_type = "t2.micro"
+  user_data = file("${path.module}/app1-install.sh")
+  tags = {
+    "Name" = "EC2 demo"
   }
+ }
