50fe2b0e7efa077a4e21a680b84f71799601d536
it.soldier999 <it.soldier999@gmail.com>
2024-05-11 23:02:39 +0530

    vpc name changed

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/terraform-manifests/c5-ec2instance.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/terraform-manifests/c5-ec2instance.tf
@@ -3,10 +3,10 @@ resource "aws_instance" "test_ec2VM" {
   ami = data.aws_ami.amzlinux2.id
   #instance_type = var.instance_type
   instance_type = var.instance_type_list[1]  # For List
-  #nstance_type = var.instance_type_map["prod"]  # For Map
+  #instance_type = var.instance_type_map["prod"]  # For Map
   user_data = file("${path.module}/app1-install.sh")
   key_name = var.instance_keypair
-  vpc_security_group_ids = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id   ]
+  vpc_security_group_ids = [ aws_security_group.vpc-ssh1.id, aws_security_group.vpc-web1.id   ]
   count = 2
   tags = {
     "Name" = "Count-Demo-${count.index}"
