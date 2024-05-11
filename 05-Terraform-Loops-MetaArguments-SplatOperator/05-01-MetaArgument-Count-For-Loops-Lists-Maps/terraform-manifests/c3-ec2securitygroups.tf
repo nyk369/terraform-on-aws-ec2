e10ff8d0a167320002de9f93eec855c964e1799e
it.soldier999 <it.soldier999@gmail.com>
2024-05-11 23:35:35 +0530

    k

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/terraform-manifests/c3-ec2securitygroups.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/terraform-manifests/c3-ec2securitygroups.tf
@@ -1,6 +1,6 @@
 # Create Security Group - SSH Traffic
-resource "aws_security_group" "vpc-ssh" {
-  name        = "vpc-ssh"
+resource "aws_security_group" "vpc-ssh2" {
+  name        = "vpc-ssh2"
   description = "Dev VPC SSH"
   ingress {
     description = "Allow Port 22"
@@ -19,13 +19,13 @@ resource "aws_security_group" "vpc-ssh" {
   }
 
   tags = {
-    Name = "vpc-ssh"
+    Name = "vpc-ssh2"
   }
 }
 
 # Create Security Group - Web Traffic
-resource "aws_security_group" "vpc-web" {
-  name        = "vpc-web"
+resource "aws_security_group" "vpc-web2" {
+  name        = "vpc-web2"
   description = "Dev VPC Web"
   ingress {
     description = "Allow Port 80"
@@ -50,7 +50,7 @@ resource "aws_security_group" "vpc-web" {
   }
 
   tags = {
-    Name = "vpc-web"
+    Name = "vpc-web2"
   }
 }
 
