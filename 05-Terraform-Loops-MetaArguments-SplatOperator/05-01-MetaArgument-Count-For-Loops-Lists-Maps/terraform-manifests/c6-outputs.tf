72754ccb092d1b2aa47e58ab1332075ca5b5a4c7
it.soldier999 <it.soldier999@gmail.com>
2024-05-11 22:39:45 +0530

    splat operators

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/terraform-manifests/c6-outputs.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/terraform-manifests/c6-outputs.tf
@@ -9,32 +9,33 @@
 
 # Output - For Loop with List
 output "for_output_list" {
-  description = "For Loop with List"
-  value = [for instance in aws_instance.myec2vm: instance.public_dns]
+  description = "For Loop with list"
+  value = [for instance in aws_instance.test_ec2VM: instance.public_dns]
+  
 }
 
 # Output - For Loop with Map
 output "for_output_map1" {
-  description = "For Loop with Map"
-  value = {for instance in aws_instance.myec2vm: instance.id => instance.public_dns}
+  description = "For loop with map"
+  value = {for instance in aws_instance.test_ec2VM: instance.id => instance.public_dns}
+  
 }
-
 # Output - For Loop with Map Advanced
 output "for_output_map2" {
-  description = "For Loop with Map - Advanced"
-  value = {for c, instance in aws_instance.myec2vm: c => instance.public_dns}
+  description = "for loop with map - Advanced"
+  value = {for c, instance in aws_instance.test_ec2VM: c => instance.public_dns}
+  
 }
 
 # Output Legacy Splat Operator (Legacy) - Returns the List
-/*
+
 output "legacy_splat_instance_publicdns" {
   description = "Legacy Splat Operator"
-  value = aws_instance.myec2vm.*.public_dns
+  value = aws_instance.test_ec2VM.*.public_dns
+  
 }
-*/
-
 # Output Latest Generalized Splat Operator - Returns the List
 output "latest_splat_instance_publicdns" {
-  description = "Generalized latest Splat Operator"
-  value = aws_instance.myec2vm[*].public_dns
+  description = "Generalized splat Expression"
+  value = aws_instance.test_ec2VM[*].public_dns
 }
