54d08babdb987d20cc01c132a57f401824863022
it.soldier999 <it.soldier999@gmail.com>
2024-05-17 17:41:26 +0530

    created until ec2 private instance upgrade

--- 07-AWS-EC2Instance-and-SecurityGroups/terraform-manifests/c7-04-ec2instance-private.tf
+++ 07-AWS-EC2Instance-and-SecurityGroups/terraform-manifests/c7-04-ec2instance-private.tf
@@ -1,6 +1,7 @@
 # AWS EC2 Instance Terraform Module
 # EC2 Instances that will be created in VPC Private Subnets
 module "ec2_private" {
+  depends_on = [ module.vpc ]
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "5.6.1"
 
@@ -9,12 +10,15 @@ module "ec2_private" {
   instance_type = var.instance_type
   user_data = file("${path.module}/apache-install.sh")
   key_name = var.instance_keypair
-  vpc_security_group_ids = [module.public_bastion_sg.this_security_group_id]
-  subnet_id= [
-    module.vpc.private_subnets[0],
-    module.vpc.private_subnets[1]
-  ]
-  instance_count = var.private_instance_count
+  #subnet_id= [module.vpc.private_subnets[0],module.vpc.private_subnets[1]
   tags = local.common_tags
+#Updates 
+  vpc_security_group_ids = [module.private_sg.security_group_id]
+  for_each = toset(["0", "1"])
+  subnet_id = element(module.vpc.private_subnets, tonumber(each.key))
+
 }
+
+
+
 
