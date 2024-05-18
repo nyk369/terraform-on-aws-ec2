347f70329c06fc1eda439cf01738968f0dc6ece1
it.soldier999 <it.soldier999@gmail.com>
2024-05-18 13:11:16 +0530

    errors in c7-03, 04 resolved

--- 07-AWS-EC2Instance-and-SecurityGroups/terraform-manifests/c7-02-ec2instance-outputs.tf
+++ 07-AWS-EC2Instance-and-SecurityGroups/terraform-manifests/c7-02-ec2instance-outputs.tf
@@ -13,7 +13,7 @@ output "ec2_bastion_public_ip"{
 }
 output "ec2-private_instance_ids"{
   description = "List of IDs of instances"
-  value = [for ec2private in module.module.ec2_private: ec2private.id]
+  value = [for ec2private in module.ec2_private: ec2private.id]
 }
 
 output "ec2_private_ip"{
